df11273dc3ec9ded2a0cc14f45a028b38ee1bdb3
Alex Groat <alex.groat.95@gmail.com>
2022-01-12 20:40:58 +1030

    User model complete

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thoughts');
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: true,
+            // validate with regex from previous homework
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }]
+    }, 
+
+    { 
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        }
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('Users', userSchema);
+
+module.exports = Users;
