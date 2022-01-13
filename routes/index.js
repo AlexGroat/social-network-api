bd1b5b29fc48ac40c92d625004eac283e35e4427
Alex Groat <alex.groat.95@gmail.com>
2022-01-13 17:34:58 +1030

    working user routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('api', apiRoutes);
-
-router.use((req,res) => {
-    res.status(404).json({ message: 'Wrong Route!'})
-});
+router.use('/api', apiRoutes);
 
 module.exports = router;
