03817202fbd96b4df4eb4affaa1d9a30a6146955
Alex Groat <alex.groat.95@gmail.com>
2022-01-13 16:37:11 +1030

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,36 @@
 const { Users } = require('../models');
 
-// get all users
+module.exports ={
+    // get all users
+    getUsers(req, res) {
+        Users.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
 
-// get a user by ID
+    // get a user by ID
+    getSingleUser(req, res) {
+        Users.findOne({_id})
+    },
 
-// create a new user
+    // create a new user
+    createUser(req, res) {
 
-// update a single user by id
+    },
 
-// delete a user by id
+    // update a single user by id
+    updateUser(req, res) {
+
+    },
+
+    // delete a user by id
+    deleteUser(req, res) {
+        
+    }
+}
+
+
+
+
+
+
--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const thoughtSchema = require('./Thoughts');
 
 const userSchema = new Schema (
     {
